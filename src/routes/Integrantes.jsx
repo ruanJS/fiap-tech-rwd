da40ab961856a5c88cf708b9d0c9a83c7c494f19
bruno1098 <rm98470@fiap.com.br>
2023-09-12 19:48:26 -0300

    Criando a página de Integrantes

--- src/routes/Integrantes.jsx
+++ src/routes/Integrantes.jsx
@@ -1,6 +1,15 @@
-export default function Integrantes(){
-    return(
-        <>
-        </>
-    )
+export default function Integrantes() {
+  return (
+    <>
+      <h1>Integrantes</h1>
+      <ul>
+        <li>Ruan Guedes de Campos - RM551096</li>
+        <li>Pedro Henrique Ferrari Cardoso - RM550231</li>
+        <li>Douglas Cavichioli - RM551100</li>
+        <li>Ana Beatriz Bento da Silva - RM552536</li>
+        <li>Bruno da Silva Albino Antunes - RM98470</li>
+        <li>Rafael Lino - RM551577</li>
+      </ul>
+    </>
+  );
 }
