8b88fd1129f233ea3b8dc0d60cf4f80b9427cd29
rafak7 <rafael.linoseabra@gmail.com>
2023-09-12 03:14:39 -0300

    removendo home

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,14 +1,6 @@
-import React from 'react';
-import { Link } from "react-router-dom";
-
-//criando duas propagandas promocionais
-export default function Home() {
-  <Link to="/Lista/aparelho/">Ver aparelho</Link>
-  return (
-    <>
-   
-    </>
-  );
+export default function Home(){
+  <>
+  
+  
+  </>
 }
-
-
