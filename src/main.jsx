a343805a20576c080ad769b6d8d7402ab4eaef66
Pedro-Ferrari <pedrofcmp@gmail.com>
2023-09-12 13:06:47 -0300

    Arrumando imports

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,8 @@ import App from './App.jsx'
 import './App.css'
 
 import { createBrowserRouter, Router, RouterProvider } from 'react-router-dom'
-import Conteudo from './components/Conteudo.jsx'
-import Header from './components/Header.jsx'
 import Integrantes from './components/Integrantes.jsx'
 import Problema from './components/Problema.jsx'
-import Rodape from './components/Rodape.jsx'
 import Solucao from './components/Solucao.jsx'
 import Home from './routes/Home.jsx'
 
