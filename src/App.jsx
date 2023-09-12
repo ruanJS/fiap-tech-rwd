adc820a6d1fd6afa7805014f38ac37f6fe596d05
rafak7 <rafael.linoseabra@gmail.com>
2023-09-12 03:14:14 -0300

    importando Conteudo

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,13 @@ import Solucao from "./components/Solucao"
 import Integrantes from "./components/Integrantes"
 import Rodape from "./components/Rodape"
 import { createElement } from "react"
+import Conteudo from "./components/Conteudo"
 
 export default function App() {
   return (
     <div>
       <Header/>
+      <Conteudo />
       <Home/>
       <Problema/>
       <Solucao/>
