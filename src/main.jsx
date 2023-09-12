c33d11bedbe47a85c74018f2d42d8de0a80ad3c9
Pedro-Ferrari <pedrofcmp@gmail.com>
2023-09-12 12:57:46 -0300

    Importando para main

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './App.css'
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Conteudo from './components/Conteudo.jsx'
+import Header from './components/Header.jsx'
+import Integrantes from './components/Integrantes.jsx'
+import Problema from './components/Problema.jsx'
+import Rodape from './components/Rodape.jsx'
+import Solucao from './components/Solucao.jsx'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
