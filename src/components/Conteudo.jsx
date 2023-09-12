37c174d24c4d7cd69de837c08add712816ca3990
rafak7 <rafael.linoseabra@gmail.com>
2023-09-12 03:14:23 -0300

    add conteudo ao Conteudo

--- src/components/Conteudo.jsx
+++ src/components/Conteudo.jsx
@@ -1,14 +1,28 @@
-import App from "../App"
-
 export default function Conteudo(){
     return(
         <> 
-            <h1>PORTO SEGURO</h1>
-            <p>A Porto iniciou suas atividades em 1945, com apenas 50 funcionários. Atualmente, é composta por 27 empresas com
+            <div className="label">
+                <h1>PORTO SEGURO</h1>
+                <p>A Porto iniciou suas atividades em 1945, com apenas 50 funcionários. Atualmente, é composta por 27 empresas com
         quase 14 mil funcionários. Reconhecida como uma das maiores seguradoras do País, a Porto hoje é mais que uma
         seguradora, é um ecossistema de soluções de serviços de proteção com tecnologia embarcada, para melhorar e
         facilitar a experiência do cliente. Com mais de 70 anos de mercado, a atuação da companhia se concentra hoje em
         quatro pilares estratégicos de negócio: Seguros, Saúde, Negócios e Financeiros.</p>
-        </>
+        <img src="./public/img-guincho.png" alt="" />
+            </div>
+
+
+            <div className="label-2">
+                <h1>MODAL</h1>
+                <p>A Porto é mais do que uma seguradora: é um ecossistema com todas as soluções para transformar sonhos em
+        realidades diárias. Com mais de 75 anos de mercado, possui hoje três verticais de negócios: Porto Seguros, Porto
+        Saúde e Porto Seguro Bank, formando, assim, um conjunto de soluções de proteção para os seus momentos que vão
+        desde o dia a dia até o grande dia. A sua missão é assumir riscos e prestar serviços, por meio de um atendimento
+        familiar que supere expectativas, garantindo agilidade a custos competitivos com responsabilidade social e
+        ambiental.</p>
+        <img src="./public/img-reboque.png" alt="" />
+            </div>
+        </> 
+
     )
 }
