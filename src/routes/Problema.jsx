e6289c60711af8dc9bce71c22a08438c05861311
bruno1098 <rm98470@fiap.com.br>
2023-09-12 19:48:40 -0300

    Criando a página de problemas

--- src/routes/Problema.jsx
+++ src/routes/Problema.jsx
@@ -1,6 +1,28 @@
-export default function Problemas(){
-    return(
-        <>
-        </>
-    )
+export default function Problemas() {
+  return (
+    <>
+      <h1>Problema</h1>
+      <p>
+        Durante a análise dos desafios propostos para o Challenge 2023 - Porto
+        Seguro, identificámos um conjunto de questões obtidas pelo desafio
+        proposto, por falta de informações transmitida a Porto no pedido de
+        reboques adequados à procura; uma modalidade; um veículo modificado
+        cujas características básicas são alteradas para determinar o modo
+        correto; uma rota que não atende às necessidades do veículo de
+        transporte. Para isso desenvolvemos, uma solução criativa e eficiente
+        para esse problema enfrentado por nossos clientes.
+      </p>
+      <img src="./public/img-guincho.png" alt="" />
+      <h1>Desafio</h1>
+      <p>
+        É possível identificar uma série de desafios quando o assunto é a
+        comunicação entre a empresa e o cliente. A ausência de informações
+        precisas quando um pedido de reboque é realizado, pode resultar em
+        complicações no envio do modal/guincho para solucionar o problema.
+        Portanto, é necessário que as informações fornecidas pelo cliente sejam
+        concisas proporcionando um atendimento mais rápido e preciso.
+      </p>
+      <img src="./public/img-guincho.png" alt="" />
+    </>
+  );
 }
