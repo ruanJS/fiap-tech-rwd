c05997a57b025cf9ef2ed6ce55c917a4b71a0b85
bruno1098 <rm98470@fiap.com.br>
2023-09-12 19:47:35 -0300

    retirando o conteudoLista

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import React from "react"
 import Header from "./components/Header"
-import Home from "./routes/Home"
 import Rodape from "./components/Rodape"
 import Home from "./components/Home"
-import ConteudoLista from "./components/ConteudoLista"
+//import ConteudoLista from "./components/ConteudoLista"
 
 export default function App() {
   return (
     <div>
       <Header/>
       <Home />
-      <ConteudoLista/>
+      {/* {*<ConteudoLista/>*} */}
       <Rodape/>
     </div>
   );
