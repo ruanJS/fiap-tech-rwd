5f3a5969fbdcc086d9382f5898811f116cc8d79c
bruno1098 <rm98470@fiap.com.br>
2023-09-12 19:47:50 -0300

    Alterando o link da Home

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Integrantes from './routes/Integrantes.jsx'
 import Problema from './routes/Problema.jsx'
 import Solucao from './routes/Solucao.jsx'
-import Home from './routes/Home.jsx'
+import Home from './components/Home.jsx'
 import Erro404 from './routes/Erro404.jsx'
 
 
